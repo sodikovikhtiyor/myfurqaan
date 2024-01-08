9fd02a94c5ed6245e4b5a2e1a5d19d2a60797e4d
Ikhtiyor <ihtiyor2655742@gmail.com>
2024-01-09 01:38:21 +0500

    first message

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import { ChakraProvider } from "@chakra-ui/react";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <ChakraProvider>
+        
+      </ChakraProvider>
+    </>
+  );
+}
+
+export default App;
